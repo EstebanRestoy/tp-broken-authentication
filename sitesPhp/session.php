e1be07c828fddd3dbaba3b3fd6e9f79b23cb4bc0
EstebanRestoy <esteban.restoy@etu.uca.fr>
2021-06-11 09:45:36 +0200

    [ADD] réponses

--- sitesPhp/session.php
+++ sitesPhp/session.php
@@ -1,13 +1,13 @@
 <?php
 $a = session_id("c3c1e5tun1dd3s3s51on");
-if(empty($a)) session_start();
+        if(empty($a)) session_start();
 ?>
 
 <div style="display: flex; margin: auto; flex-direction: column; width: 100%; align-items: center">
     <h1>Level 1</h1>
     <p>Trouvez deux solutions pour retrouvez l'identifiant de session.</p>
     <p>Le formulaire peut vous aider.</p>
-    <form name="s1" action="session1.php" method="POST"
+    <form name="s1" action="session.php" method="POST"
           style="display: flex; flex-direction: column">
         <input name="msg" placeholder="Votre message"/>
         <button type="submit" style="margin-top: 0.25rem;">Envoyer</button>
